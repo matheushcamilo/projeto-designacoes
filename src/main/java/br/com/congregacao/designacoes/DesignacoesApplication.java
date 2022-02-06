3ca98200b27d12b67801f954d26d627069686b86
Mathus Araujo <matheushcamilo1997@gmail.com>
2022-02-06 14:22:24 -0300

    Geração de projeto Spring Boot pelo Spring Initializr

---
+++ src/main/java/br/com/congregacao/designacoes/DesignacoesApplication.java
@@ -0,0 +1,13 @@
+package br.com.congregacao.designacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignacoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/congregacao/designacoes/DesignacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.congregacao.designacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
